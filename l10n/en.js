1a936156e8652343d16c960cab8b9fbc154b3b0b
Chang Liu <chang@quadservices.com.au>
2015-02-09 16:51:01 +1100

    Initial commit.

---
+++ l10n/en.js
@@ -0,0 +1,123 @@
+{
+    "TITLE":"Chang Liu, Full-stack .NET/AngularJS Developer",
+    "header" : {
+        "logo" : {
+            "content" : {
+                 "FIRSTNAME" : "Chang",
+                 "LASTNAME": "Liu",
+                 "POSITION":"Full-stack .NET/AngularJS Developer" 
+            }
+        },
+        "nav" : {
+            "menu" : {
+                "LANGUAGE" : "English",
+                 "PROFILE": "Profile",
+                 "RESUME":"Resume",
+                 "PORTFOLIO":"Portfolio",
+                 "BLOG":"Blog",
+                 "CONTACT":"Contact"    
+                 }
+             
+        },
+          "COPYRIGHT":"Chang Liu"
+    },
+    "profile":{
+        "topBar":{
+            "content":{
+                "TITLE":"Check my recent activities !"
+            }
+        },
+        "content":{
+            "title":{
+                "PREFIX":"I am ",
+                "NAME":"Chang Liu",
+                "TITLE":"full-stack ",
+                "POSITION":".NET/AngularJS Developer"
+            },
+            "button":{
+                "LEFT":"My blog",
+                "RIGHT":"Contact me"
+            } 
+        },
+         "bottomBar":{
+             "ADDRESS":"16/38 Shoreline Drive Rhodes 2138 NSW, Australia",
+             "EMAIL":"3dslayer@gmail.com",
+             "PHONE":"+(61)405849585"
+         }
+    },
+    "resume":{
+        "aboutMe":{
+            "TITLE":"Who am I?",
+            "SUBTITLE":"A hard worker, a quick learner, a critical problem solver and a passionate developer with solid programming skills.",
+            "description":{
+                "P1":"Master's degree of IT from University of New South Wales (UNSW) with Distinction average (WAM 82.5). The winner of 'CSE Postgraduate Performance Prize' in 2 consecutive semesters respectively in 2012 Semester 2 and 2013 Semester 1.",
+                "P2":"Achieved Microsoft Certification in Web Applications (MCSD), Microsoft Certified Professional (MCPS) and Microsoft Certified HTML5 with JavaScript and CSS3 Specialist.",
+                "P3":"Familiar with application development across different platforms including web, mobile and desktop."
+            },
+            "button":{
+                "LEFT":"See my portfolio",
+                "RIGHT":"Contact me"
+            }
+        },
+        "skill":{
+            "TITLE":"Skills"
+        },
+        "specialities":{
+            "TITLE":"Specialities",
+            "spec1":{
+                "TITLE":"Web Application",
+                "SUBTITLE":"AngularJS single page application,Responsive UI Design, ASP.NET MVC application"
+            },
+            "spec2":{
+                "TITLE":"API Development",
+                "SUBTITLE":"WebAPI 2 and NodeJS Restful API, WCF"
+            },
+            "spec3":{
+                "TITLE":"Mobile Application",
+                "SUBTITLE":"Ionic, PhoneGap, Apache Cordova, Visual studio 2013 Multi-Device Hybrid App"
+            },
+            "spec4":{
+                "TITLE":"DataBase",
+                "SUBTITLE":"Entity Framework, Sql Server 2012, LINQ to SQL, SQL/T-SQL"
+            }
+        },
+        "eduAndJobs":{
+            "TITLE":"Education & Jobs",
+            "event1":{
+                "DATE":"July 2014 - Present",
+                "NAME":"Full Stack .Net Developer",
+                "DESCRIPTION":"Quad Services Pty Ltd"
+            },
+            "event2":{
+                "DATE":"November 2013 - June 2014",
+                "NAME":"Software Developer",
+                "DESCRIPTION":"PicNet Pty Ltd"
+            },
+            "event3":{
+                "DATE":"October 2013 - December 2013",
+                "NAME":"Software Development Lead",
+                "DESCRIPTION":"VVV solutions"
+            },
+            "event4":{
+                "DATE":"2012 - 2013",
+                "NAME":"Master's degree, Information Technology",
+                "DESCRIPTION":"University of New South Wales"
+            }
+        },
+        "language":{
+            "TITLE":"Languages",
+            "LANGUAGE1":"English",
+            "LANGUAGE2":"Chinese"
+        },
+        "hobbies":{
+            "TITLE":"Hobbies & Interests",
+            "HOBBY1":"Football",
+            "HOBBY2":"Video Game",
+            "HOBBY3":"New technologies"
+        }
+    },
+    "contact":{
+        "TITLE":"Do you want to hire me?",
+        "DOWNLOAD_BUTTON":"Download my CV"
+    }
+}
